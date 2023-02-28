007f2ef2392c1943ae427f2164406b3c73e7139a
车宇勋 <270928763@qq.com>
2023-02-28 16:59:06 +0800

    订单服务搭建

---
+++ shop-order-server/src/main/java/cn/wolfcode/OrderSerer.java
@@ -0,0 +1,11 @@
+package cn.wolfcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSerer {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSerer.class,args);
+    }
+}
